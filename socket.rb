a485ea20415be5ec00e284288c734feb6cb72d5f
carusocr <carusocr@ldc.upenn.edu>
2014-09-05 14:49:07 -0400

    looking into client

--- socket.rb
+++ socket.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+#look into websocket client
+# https://github.com/mwylde/em-websocket-client
 
 require 'em-websocket'
 require 'uuid'
@@ -9,14 +11,14 @@ uuid = UUID.new
 EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
   ws.onopen do
     puts "WebSocket opened"
-    AMQP.connect(:host => '127.0.0.1') do |connection, open_ok|
-      AMQP::Channel.new(connection) do |channel, open_ok|
-        channel.queue(uuid.generate, :auto_delete => true).bind(channel.fanout("twitter")).subscribe do |t|
-          puts 'got a tweet'
-          ws.send t
-        end
-      end
-    end
+#    AMQP.connect(:host => '127.0.0.1') do |connection, open_ok|
+#      AMQP::Channel.new(connection) do |channel, open_ok|
+#        channel.queue(uuid.generate, :auto_delete => true).bind(channel.fanout("twitter")).subscribe do |t|
+#          puts 'got a tweet'
+#          ws.send t
+#        end
+#      end
+#    end
   end
   ws.onclose do
     puts "WebSocket closed"
