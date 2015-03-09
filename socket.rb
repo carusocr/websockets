7f7bc32e0522f59b83fb93cb534469e0efaae7c6
carusocr <carusocr@ldc.upenn.edu>
2015-03-09 15:23:48 -0400

    changed ports, socket working now

--- socket.rb
+++ socket.rb
@@ -9,32 +9,27 @@ require 'bunny'
 conn = Bunny.new
 conn.start
 ch = conn.create_channel
-#ch = conn.default_channel
 cq = ch.queue("command")
 tq = ch.queue("tweets")
-#cq.publish("Testing command queue")
-#sleep 1
-#tq.publish("Testing tweet queue")
-#sleep 1
-#conn.close
-#exit
-EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
-  ws.onopen do
-    puts "WebSocket opened"
-    conn = Bunny.new
-    conn.start
-    #ch = conn.default_channel
-    q = ch.queue("tweets")
-    q.subscribe(:block => true) do |delivery_info, properties, body|
-      puts "Received tweet\n"
-      encoded_tweet=body.force_encoding("iso-8859-1").force_encoding("utf-8")
-      ws.send encoded_tweet
+EM.run {
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 8567) do |ws|
+    ws.onopen do
+      puts "WebSocket opened"
+#      conn = Bunny.new
+#      conn.start
+      q = ch.queue("tweets")
+      q.subscribe(:block => true) do |delivery_info, properties, body|
+        puts "Received tweet\n"
+        encoded_tweet=body.force_encoding("iso-8859-1").force_encoding("utf-8")
+        puts encoded_tweet
+        ws.send encoded_tweet
+      end
+      q.publish("Test") 
     end
-    q.publish("Test") 
+    ws.onclose do
+      ws.close(code = nil, body = nil)
+      puts "WebSocket closed"
+      exit
+    end
   end
-  ws.onclose do
-    ws.close(code = nil, body = nil)
-    puts "WebSocket closed"
-    exit
-  end
-end
+}
