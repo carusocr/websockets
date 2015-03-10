abe06d5be9f01a3dd46e1667eea5709d244e3e6a
carusocr <carusocr@ldc.upenn.edu>
2015-03-10 15:26:01 -0400

    troubleshooting

--- ts.rb
+++ ts.rb
@@ -1,31 +1,29 @@
 #!/usr/bin/env ruby
 #look into websocket client
 # https://github.com/mwylde/em-websocket-client
+# troubleshooting socket.rb: socket and tweetstream communicate, but map doesn't get msgs
 
 require 'em-websocket'
-require 'uuid'
 require 'bunny'
 
 conn = Bunny.new
 conn.start
 ch = conn.create_channel
-cq = ch.queue("command")
 tq = ch.queue("tweets")
+
+#=begin
+#THIS WORKS AND GENERATES AN ALERT IN MAP.HTML
+
 EM.run {
   EM::WebSocket.run(:host => "127.0.0.1", :port => 8567) do |ws|
-  #EM::WebSocket.run(:host => "127.0.0.1", :port => 8080) do |ws|
     ws.onopen do
       puts "WebSocket opened"
-#      conn = Bunny.new
-#      conn.start
-      q = ch.queue("tweets")
-      q.subscribe(:block => true) do |delivery_info, properties, body|
-        puts "Received tweet\n"
-        encoded_tweet=body.force_encoding("iso-8859-1").force_encoding("utf-8")
-        puts encoded_tweet
-        ws.send encoded_tweet
-      end
-      q.publish("Test") 
+      ws.send "test"
+      #commenting this out allows test to go through, uncommenting prevents. ?!?
+      #tq.subscribe(:block=>true) do |delinfo, properties, body|
+      #  puts "Got one! Sending test to map..."
+      #  ws.send "test"
+      #end
     end
     ws.onclose do
       ws.close(code = nil, body = nil)
@@ -34,3 +32,4 @@ EM.run {
     end
   end
 }
+#=end
